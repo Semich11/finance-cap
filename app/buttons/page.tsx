2f58deb2fa2456983e8cda919db1ef05989cb3d5
semich11 <chima11christopher@gmail.com>
2025-04-23 09:12:25 +0100

    Setup default Button.

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
 const ButtonsPage = () => {
     return(
-        <div>
-            Button Pages!
+        <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
+            <Button>Primary</Button>
+            <Button>Primary Outline</Button>
         </div>
     )
 }
+
+export default ButtonsPage;
