57be8691aed64bb7f9d052021ade3a13066874c3
semich11 <chima11christopher@gmail.com>
2025-04-25 13:48:26 +0100

    Added sidebar items.

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,15 +1,34 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { SidebarItem } from "./sidebar-item";
 
 type Props = {
-    className?: string,
-}
+  className?: string;
+};
 
-export const Sidebar = ({className}:Props) => {
-    return(
-        <div className={cn("flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
-            className
-        )}>
-            Sidebar
+export const Sidebar = ({ className }: Props) => {
+  return (
+    <div
+      className={cn(
+        "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        className
+      )}
+    >
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/mascot.png" height={40} width={40} alt="Mascot" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Finance-app
+          </h1>
         </div>
-    )
-}
+      </Link>
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem label="Learn" iconSrc="/learn.png" href="/learn" />
+        <SidebarItem label="Leaderboard" iconSrc="/learderboard.png" href="/learderboard" />
+        <SidebarItem label="Quest" iconSrc="/quest.png" href="/quest" />
+        <SidebarItem label="Shop" iconSrc="/shop.png" href="/shop" />
+      </div>
+    </div>
+  );
+};
