ac101594075b1411fc2a812d5e8ec5405104ab51
semich11 <chima11christopher@gmail.com>
2025-04-27 13:04:53 +0100

    Added and still working on the FeedWrapper component.

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,14 @@
+import { StickyWrapper } from "@/components/sticky-wrapper";
+
 const LearnPage = () => {
     return(
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
                 My sticky sidebar
             </StickyWrapper>
+            <FeedWrapper>
+                My feed
+            </FeedWrapper>
         </div>
     )
 }
