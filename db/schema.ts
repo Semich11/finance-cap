8a580e0243920c6d38c98f49cbc446e995d4a097
semich11 <chima11christopher@gmail.com>
2025-04-29 12:38:03 +0100

    Created user progress table.

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,16 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, integer } from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses", {
     id: serial("id").primaryKey(),
     title: text("title").notNull(),
     imgSrc: text("image_src").notNull(),
+})
+
+export const userProgress = pgTable("user_progress", {
+    userId: text("user_id").primaryKey(),
+    userName: text("user_name").notNull().default("User"),
+    userImageSrc: text("user_image_src").notNull().default("/mascot.png"),
+    activeCourseId: integer("active_course_id").references(() => courses.id, { onDelete: "cascade" }),
+    hearts: integer("hearts").notNull().default(5),
+    points: integer("points").notNull().default(0),
 })
