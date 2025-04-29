1276936faa8d6f7da3f44fcd8855fb543e36aefb
semich11 <chima11christopher@gmail.com>
2025-04-29 14:53:12 +0100

    I'm displaying all the courses on the UI, and I just started working on the Loding page.

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,15 +1,30 @@
+import { redirect } from "next/navigation";
+
+import { getUserProgress } from "@/db/queries";
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { UserProgress } from "@/components/user-progress";
 
 import { Header } from "./header";
 
-const LearnPage = () => {
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+
+  const [
+    userProgress
+  ] = await Promise.all([
+    userProgressData
+  ]);
+
+  if (!userProgress || !userProgress.activeCourse){
+    redirect("/courses")
+  }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{title: "Investment", imageSrc: "/return.png"}}
+          activeCourse={{ title: "Investment", imageSrc: "/return.png" }}
           hearts={5}
           points={100}
           hasActiveSubscription={false}
@@ -17,7 +32,6 @@ const LearnPage = () => {
       </StickyWrapper>
       <FeedWrapper>
         <Header title="Investment" />
-
       </FeedWrapper>
     </div>
   );
