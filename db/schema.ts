96f365454517f38209c36292be00821722368609
semich11 <chima11christopher@gmail.com>
2025-04-30 17:47:49 +0100

    Created schema for lessons table.

--- db/schema.ts
+++ db/schema.ts
@@ -27,6 +27,12 @@ export const unitRelations = relations(units, ({many, one }) => ({
   })
 }));
 
+export const lessons = pgTable("lessons", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  unitId: integer("unit_id").references(() => units.id, {onDelete: "cascade"}).notNull(),
+  order: integer("order").notNull(),
+});
 
 
 export const userProgress = pgTable("user_progress", {
