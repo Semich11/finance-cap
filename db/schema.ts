29ccd958b418ce5e5a85a8dd7b6094a79351aaf0
semich11 <chima11christopher@gmail.com>
2025-05-01 11:32:14 +0100

    Created schema for challengeOptions table.

--- db/schema.ts
+++ db/schema.ts
@@ -68,6 +68,13 @@ export const challengeOptions = pgTable("challengeOptions", {
   audioSrc: text("audio_src"),
 });
 
+export const challengeOptionsRelations = relations(challengeOptions, ({ one }) => ({
+  challenge: one(challenges, {
+    fields: [challengeOptions.challengeId],
+    references: [challenges.id],
+  }),
+}));
+
 export const userProgress = pgTable("user_progress", {
   userId: text("user_id").primaryKey(),
   userName: text("user_name").notNull().default("User"),
