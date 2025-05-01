aa02ed41a76c749ba63469a2643f1469cb3dc467
semich11 <chima11christopher@gmail.com>
2025-05-01 11:55:03 +0100

    Update challengesRelations to include many challengeProgress.

--- db/schema.ts
+++ db/schema.ts
@@ -58,6 +58,7 @@ export const challengesRelations = relations(challenges, ({ one, many }) => ({
     references: [lessons.id],
   }),
   challengeOptions: many(challengeOptions),
+  challengeProgress: many(challengeProgress),
 }));
 
 export const challengeOptions = pgTable("challengeOptions", {
